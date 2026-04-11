6a0cb56bb57d5be62dea8f4c0952af46f8813b6e
sivachaitanya7867 <sivachaitanya.7867@gmail.com>
2026-04-10 21:18:29 -0500

    complete raw table structure

--- sql/ddl/002_raw_tables.sql
+++ sql/ddl/002_raw_tables.sql
@@ -1,4 +1,9 @@
 CREATE TABLE raw.crypto_market_raw (
     ingestion_id SERIAL PRIMARY KEY,
-    coin_id TEXT
+    coin_id TEXT,
+    symbol TEXT,
+    fetched_at TIMESTAMP,
+    page_number INT,
+    raw_payload JSONB,
+    record_hash TEXT
 );
